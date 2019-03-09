c206826300b232d090c733b0a3a13d499cee8441
Erick Ribeiro <erick.ribeiro.16@gmail.com>
2019-03-09 01:28:11 -0400

    feat(spread) Suporte a feature spread
    A feature spread não é suportada nativamente pelo babel, por isso utilizamos um plugin para contornar esse problema.

--- exercicio_webpack/ex/index.js
+++ exercicio_webpack/ex/index.js
@@ -1,4 +1,17 @@
 import Pessoa from './pessoa'
 
+const produto = {
+    nome: 'Caneta Bic Preta',
+    preco: 0.50,
+    desconto: 0.10
+};
+const clone = (obj) => {
+    return {...obj}
+};
+
 const pessoa = new Pessoa('Erick!!');
 console.log(pessoa.toString());
+
+const novoProduto = clone(produto);
+novoProduto.nome = 'Caneta Bic Azul';
+console.log(produto, novoProduto);
